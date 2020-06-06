31a560debeddbd481562fc9de29cd9dafe4e5dd9
Agostina <Agosss@MacBook-Air-de-Agostina.local>
2020-06-06 13:13:20 -0300

    resultados linkeados

--- java script/resultados.js
+++ java script/resultados.js
@@ -1,6 +1,21 @@
 window.addEventListener("load", function(){
     let queryString= new URLSearchParams(location.search)
     let codigoDelBuscador= queryString.get("buscador")
+    
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=artist:"+codigoDelBuscador)
+    .then(function(respuestaSearchArtista){
+        return respuestaSearchArtista.json()
+    })
+    .then(function(infoSearchArtista){
+        console.log(infoSearchArtista)
+        let infoBuscadorArtista= infoSearchArtista.data
+        console.log(infoBuscadorArtista)
+
+
+
+    })
+
+
     fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q="+codigoDelBuscador)
     .then(function(respuestaSearch){
         return respuestaSearch.json()
@@ -18,17 +33,19 @@ window.addEventListener("load", function(){
         let idBuscado= cadaBuscado.id
         let imgBuscado= cadaBuscado.album.cover
         let nombreArtistaBuscado = cadaBuscado.artist.name
+        let idArtista= cadaBuscado.artist.id
         document.querySelector(".container-search").innerHTML+= "<div class=div-search>"+
         
-        "<h1 class=titulo-search>"+nombreBuscador+"</h1>"+
-        "<h2 class=artista-search>"+nombreArtistaBuscado+"</h2>"+
-        "<img class=img-search src='"+imgBuscado+"'"+ "</div>"
+        "<a href='tracks.html?idDelTrack="+idBuscado+"'>"+"<h1 class=titulo-search>"+nombreBuscador+"</h1>"+"</a>"+
+        "<a href='artistas.html?idDelArtista="+idArtista+"'>"+"<h2 class=artista-search>"+nombreArtistaBuscado+"</h2>"+"</a>"+
+        "<a href='tracks.html?idDelTrack="+idBuscado+"'>"+"<img class=img-search src='"+imgBuscado+"'"+"</a>"+"<i class='far fa-play-circle iconossearch '></i>"+"<i class='fas fa-heart iconossearch '></i>"+ "</div>"
             
         }
         
         
 
     })
+    
 
 
 
