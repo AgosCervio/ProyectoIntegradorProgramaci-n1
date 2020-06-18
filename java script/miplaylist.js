0fbfdc7dc08f61e33666614c723be4f7edb51c3d
Agostina <Agosss@Air-de-Agostina.fibertel.com.ar>
2020-06-17 22:09:38 -0300

    playlist

--- java script/miplaylist.js
+++ java script/miplaylist.js
@@ -28,13 +28,23 @@
                   let album = track.album.title
                   let idDeAlbum = track.album.id
                   let imagen = track.album.cover
+                  let idTrack= track.id
                   
-            document.querySelector(".listacanciones").innerHTML += `<li><div class="sectioncancionesfoto"><img class=cancion src="`+ imagen+` "></div><div class="sectioninfocanciones"><p class=nombrecancion>`+nombre+`</p><a href="artistas.html?idDelArtista=`+idArtista +`"><p class=ipervincuos>`+artista+` </p> </a><a  href="albums.html?idDeAlbum=`+idDeAlbum+`"><p class="ipervincuosalbum">`+album+`</p></a>   <p class="duracion">`+duracion+`</p> <p class="iconotracks"> PLAY </p></div></li>`
+            document.querySelector(".listacanciones").innerHTML += `<li><div class="sectioncancionesfoto"><img class=cancion src="`+ imagen+` "></div><div class="sectioninfocanciones"><p class=nombrecancion>`+nombre+`</p><a href="artistas.html?idDelArtista=`+idArtista +`"><p class=ipervincuos>`+artista+` </p> </a><a  href="albums.html?idDeAlbum=`+idDeAlbum+`"><p class="ipervincuosalbum">`+album+`</p></a>   <p class="duracion">`+duracion+`</p> <p id="`+idTrack+ `class="iconotracks"> PLAY </p></div></li>`
 
-                  ""
-              
+                  
+              let play = document.querySelectorAll("iconotracks")
+              for (let index = 0; index < play.length; index++) {
+                  const cadaPlay = play[index];
+                  let id = cadaPlay.getAttribute("id")
+                  cadaPlay.addEventListener("click", function(){
+                      alert("anda")
+                      document.querySelector(".reproductortrack").innerHTML= "<iframe class=iframe id=dzplayer dztype=dzplayer src='http://developers.deezer.com/us/plugins/player?playlist=true&width=700&height=240&autoplay=false&type=tracks&id="+id+"' scrolling=no frameborder=0 style=border:none; overflow:hidden; width=700 height=40 allowTransparency=true></iframe>"
+                  })
+                  
+              }
 
-              } )
+              })
        
    }
 
@@ -48,14 +58,7 @@
 
  
 
- ` <ol class="cancionesplaylist">
- <div class="divcancionesplaylist playlistcategoria"><li>Nombre</li><li>Artista</li><li>Cancion</li></div>
- <div class="divcancionesplaylist"><li class="titulocancionplaylist"><p class=nombrecancion>`+nombre+`</p></li>
- <li class="artistacancionplaylist">   <a href="artistas.html?idDelArtista=`+idArtista +`"><p class=ipervincuos>`+artista+` </p> </a> </li>
-<li><img class="imagencancionplaylist" src="`+ img+`" alt=""></li></div>
- 
-  
-</ol>`
+
 
 
   
