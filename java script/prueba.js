6916e268076fad0e3e6e67eae0ea038f8e8a40cd
Agostina <Agosss@Air-de-Agostina.fibertel.com.ar>
2020-06-19 10:06:49 -0300

    reproductor js

--- java script/prueba.js
+++ java script/prueba.js
@@ -5,10 +5,15 @@ window.addEventListener("load", function(){
         
         let idReproductor= sessionStorage.getItem("reproduccion")
         
-           reproduccionCanciones.innerHTML= "<iframe class='iframe' scrolling='no' frameborder='0' allowTransparency='true' src='https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id="+idReproductor+"&app_id=1' width='100%' height='240'></iframe>"
+            reproduccionCanciones.innerHTML= "<iframe class='iframe' scrolling='no' frameborder='0' allowTransparency='true' src='https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id="+idReproductor+"&app_id=1' width='100%' height='240'></iframe>"
+            
+        
+           
     } else{
         reproduccionCanciones.style.display="none"
     }
+
+    
     
        
 
