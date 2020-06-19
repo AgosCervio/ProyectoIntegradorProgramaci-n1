a47ceb493c965970efa7dfa858abf5ebc193d1ad
Your Name <cbur@udesa.edu.ar>
2020-06-19 11:50:37 -0300

    ultimos

--- java script/tracks.js
+++ java script/tracks.js
@@ -22,7 +22,7 @@ window.addEventListener ("load", function(){
                 const unasola = imagen[index];
                  let img = unasola.picture
                 
-                 document.querySelector (".sectioncanciones").innerHTML = `<div class="sectioncancionesfoto"><img class=cancion src="`+ img+` "></div><div class="sectioninfocanciones"><p class=nombrecancion>`+nombre+`</p><a href="artistas.html?idDelArtista=`+idArtista +`"><p class=ipervincuos>`+artista+` </p> </a><a  href="albums.html?idDeAlbum= `+idDeAlbum+`"><p class="ipervincuosalbum">`+album+`</p></a>  <p class="duracion">`+duracion+`s</p> <a class="ipervincuos `+codigoTracks+`" href="miplaylist.html"><p class="ipervincuosplaylist">AGREGAR A PLAYLIST</p></a></a>  <p class="iconotracks"> PLAY </p></div>`
+                 document.querySelector (".sectioncanciones").innerHTML = `<div class="sectioncancionesfoto"><img class=cancion src="`+ img+` "></div><div class="sectioninfocanciones"><p class=nombrecancion>`+nombre+`</p><a href="artistas.html?idDelArtista=`+idArtista +`"><p class=ipervincuos>`+artista+` </p> </a><a  href="albums.html?idDeAlbum= `+idDeAlbum+`"><p class="ipervincuosalbum">`+album+`</p></a>  <p class="duracion">`+duracion+`s</p> <a class="ipervincuos `+codigoTracks+`" href="miplaylist.html"><p id=`+codigoTracks+` class="ipervincuosplaylist">AGREGAR A PLAYLIST</p></a></a>  <p class="iconotracks"> PLAY </p></div>`
 
         
             
@@ -31,7 +31,7 @@ window.addEventListener ("load", function(){
                 const cadatoque = iconotracks[index];
                 
                 cadatoque.addEventListener("click", function(){
-                    document.querySelector (".reproductortrack").innerHTML = `<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=tracks&id=`+codigoTracks +`&app_id=1" width="300" height="300"></iframe>`
+                    document.querySelector (".reproductortrack").innerHTML = `<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=tracks&id=`+codigoTracks+`&app_id=1" width="300" height="300"></iframe>`
                 })
             
                 }
@@ -40,34 +40,31 @@ window.addEventListener ("load", function(){
         } )
         
 
-        let bottonPlay= document.querySelectorAll(".ipervincuos")
+        let bottonPlay= document.querySelectorAll(".ipervincuosplaylist")
           
-        for (let index = 0; index < bottonPlay.length; index++) {
-            const cadaBottonPLay = bottonPlay[index];
-            let idIconoCorazon= cadaBottonPLay.getAttribute("codigoTracks")
-            cadaBottonPLay.addEventListener("click", function(){
-                alert(codigoTracks)
+        
+            bottonPlay.addEventListener("click", function(){
                 
-                if (sessionStorage.getItem("codigoPlaylist")!= null) {
-                  canciones= sessionStorage.getItem("codigoPlaylist").split(",")
-                  canciones.push(codigoTracks)
-                  sessionStorage.setItem("codigoPlaylist", canciones)
-                } else{
-                    canciones= [
-
-                    ]
-                    canciones.push(codigoTracks)
-                    sessionStorage.setItem("codigoPlaylist", canciones)
-
-                }
                 
+               
+    if (sessionStorage.getItem("codigoPlaylist")!= null) {
+        canciones= sessionStorage.getItem("codigoPlaylist").split(",")
+        canciones.push(id)
+        sessionStorage.setItem("codigoPlaylist", canciones)
+      } else{
+          canciones= [
+        
+          ]
+          canciones.push(id)
+          sessionStorage.setItem("codigoPlaylist", canciones)
+      }
                 
                 
             })
             
 
 
-}
+
 
 
 
@@ -75,4 +72,4 @@ window.addEventListener ("load", function(){
    
   
 
-  
+    
